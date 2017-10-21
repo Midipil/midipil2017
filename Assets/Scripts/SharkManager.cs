556fed8d29594ae9b84782fba78645374581cf74
Baptiste Moizard <baptiste-moizard@orange.fr>
2017-10-21 20:22:33 +0200

    Fixed sun sharks

--- Assets/Scripts/SharkManager.cs
+++ Assets/Scripts/SharkManager.cs
@@ -4,34 +4,33 @@ using UnityEngine;
 
 public class SharkManager : MonoBehaviour {
 
-    public GameObject player;
-    public GameObject sharkPrefab;
-    public float corridorWidth = 16f;
-    public float zRandom = 1.5f;
-    public float startHeight = 30f;
-    public float maxSpeed = 10.0f;
+	public GameObject player;
+	public GameObject sharkPrefab;
+	public float corridorWidth = 16f;
+	public float zRandom = 1.5f;
+	public float startHeight = 30f;
+	public float maxSpeed = 10.0f;
 	public float speed = 1f;
-	public float maxFrequency = 5f;  //par secondes
+	public float maxFrequency = 5f;
+	//par secondes
 	public float frequency = 1f;
 	public bool isAttacking = false;
 	float newSpeed;
 	float newFrequency;
 	float time;
 
-	void Start()
-	{
+	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
 
-		Start (0.8f);
+		//Start (0.8f);
 	}
 
 	// Update is called once per frame
 	void Update () {
 		time += Time.deltaTime;
-        if (Input.GetKeyUp("s"))
-        {
-            SpawnShark();
-        }
+		if (Input.GetKeyUp ("s")) {
+			SpawnShark ();
+		}
 
 		if (isAttacking) {
 			if (time > frequency) {
@@ -42,32 +41,29 @@ public class SharkManager : MonoBehaviour {
 			frequency = Mathf.Lerp (frequency, newFrequency, Time.deltaTime / 100);
 		}		
 	}
-		
-	public void Start(float difficultyLevel)
-	{
+
+	public void Start (float difficultyLevel) {
 		newFrequency = maxFrequency * difficultyLevel;
 		newSpeed = maxSpeed * difficultyLevel;
 		isAttacking = true;
 	}
 
-	public void Stop()
-	{
+	public void Stop () {
 		isAttacking = false;
 	}
 
-    public void SpawnShark()
-    {
-        // Compute distance
-        float distToTravelShark = startHeight;
-        float timeToTravel = distToTravelShark / speed; // seconds
-        float distToTravelPlayer = timeToTravel * player.GetComponent<Rigidbody>().velocity.z;
+	public void SpawnShark () {
+		// Compute distance
+		float distToTravelShark = startHeight;
+		float timeToTravel = distToTravelShark / speed; // seconds
+		float distToTravelPlayer = timeToTravel * player.GetComponent<Rigidbody> ().velocity.z;
 		// Random Z
-		float rand = Random.Range(-zRandom,zRandom);
+		float rand = Random.Range (-zRandom, zRandom);
 		float newZ = player.transform.position.z + (rand);
-        // Compute shark initial position
-		Vector3 sharkPos = new Vector3(Random.Range(-corridorWidth / 2f, corridorWidth / 2f), startHeight, distToTravelPlayer+newZ);
-        // Spawn shark
-        GameObject shark = GameObject.Instantiate(sharkPrefab, sharkPos, Quaternion.identity);
-        shark.GetComponent<Shark>().speed = speed;
-    }
+		// Compute shark initial position
+		Vector3 sharkPos = new Vector3 (Random.Range (-corridorWidth / 2f, corridorWidth / 2f), startHeight, distToTravelPlayer + newZ);
+		// Spawn shark
+		GameObject shark = GameObject.Instantiate (sharkPrefab, sharkPos, Quaternion.identity);
+		shark.GetComponent<Shark> ().speed = speed;
+	}
 }
