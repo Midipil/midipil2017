9dcf3c07b8ba95236c13c7a03875d548021c8d03
Tom <matteo.fantasy@gmail.com>
2017-10-21 16:26:19 +0200

    Start using global vars

---
+++ Assets/Scripts/GlobalVars.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVars : Singleton<GlobalVars> {
+
+    protected GlobalVars() { } // guarantee this will be always a singleton only - can't use the constructor!
+
+    // Player
+    public float maxAngle = 45f;
+    public float maxSteeringForce = 1f;
+    public float speedForce = 5000f;
+    // Health points
+    public int hp = 1;
+    // Plancton
+    public float planctonDensity = 0.5f;
+
+}
