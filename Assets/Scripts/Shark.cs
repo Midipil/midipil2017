2cb143bea265c1749120a614b3296f888b9250f3
Tom <matteo.fantasy@gmail.com>
2017-10-21 13:46:39 +0200

    add basic sharks

---
+++ Assets/Scripts/Shark.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shark : MonoBehaviour {
+
+    public float speed = 10.0f; // m/s
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (this.transform.position.y > 0) {
+            this.transform.Translate(new Vector3(0f, -speed*Time.deltaTime, 0f));
+        }
+	}
+}
