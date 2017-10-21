6550146c7287f70d997ed2fe9b6c9aa2c833664c
Baptiste Moizard <baptiste-moizard@orange.fr>
2017-10-21 20:13:23 +0200

    Prepared tiles prefab

--- Assets/Scripts/TileSpawner.cs
+++ Assets/Scripts/TileSpawner.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class TileSpawner : MonoBehaviour {
 
-	public List<TileHandler> tilesPrefab = new List<TileHandler>();
+	public List<TileHandler> tilesPrefab = new List<TileHandler> ();
 	//public List<TileHandler> tiles = new List<TileHandler> ();
 	public List<Transform> tilesObj = new List<Transform> ();
-	float tileSize;
+	public float tileSize;
 	Transform playerPos;
 	TileHandler prevTile;
 	public float frontView;
@@ -17,52 +17,47 @@ public class TileSpawner : MonoBehaviour {
 			
 		playerPos = GameObject.FindGameObjectWithTag ("Player").transform;
 
-	    tileSize = tilesPrefab [0].transform.localScale.z;
+		//tileSize = tilesPrefab [0].transform.localScale.z;
 		SpawnTile ();
 	}
 
-	void Update()
-	{
+	void Update () {
 		if (prevTile != null && prevTile.gameObject.transform.position.z <= playerPos.position.z + frontView) {
 			SpawnTile ();
 		}
 
 		for (int i = 0; i < tilesObj.Count; i++) {
-			if (tilesObj [i].position.z <= playerPos.position.z - backView) 
-			{
+			if (tilesObj [i].position.z <= playerPos.position.z - backView) {
 				GameObject.Destroy (tilesObj [i].gameObject);
 				tilesObj.Remove (tilesObj [i]);
 			}
 		}
 	}
 
-	float rand (int min, int max)
-	{
-		return Mathf.Floor(Random.value * (max - min + 1)) + min;
+	float rand (int min, int max) {
+		return Mathf.Floor (Random.value * (max - min + 1)) + min;
 	}
 
-	[ContextMenu("SpawnTile")]
-	void SpawnTile()
-	{
+	[ContextMenu ("SpawnTile")]
+	void SpawnTile () {
 		int rand = Random.Range (0, tilesPrefab.Count);
 
 		TileHandler tileH = MathUtilities.Draw (tilesPrefab);	
 
-		TileHandler tile = Instantiate(tileH);
+		TileHandler tile = Instantiate (tileH);
 
 		tile.gameObject.SetActive (true);
 
 		if (prevTile == null) {
-			tile.transform.position = new Vector3(0,0,0);
-		}
-		else {
-			tile.transform.position = new Vector3(0,0,prevTile.transform.position.z + tileSize);
+			tile.transform.position = new Vector3 (0, 0, 0);
+		} else {
+			tile.transform.position = new Vector3 (0, 0, prevTile.transform.position.z + tileSize);
 		}
 
-        tile.Init(prevTile);
+		tile.Init (prevTile);
 
-        tilesObj.Add (tile.transform);
-        prevTile = tile;
-    }
+		tilesObj.Add (tile.transform);
+		prevTile = tile;
+	}
 
 }
