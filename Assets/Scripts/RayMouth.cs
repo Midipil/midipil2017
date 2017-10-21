d671a0d3771d493a4e14efaf2a4d35084e269505
Thibaut Dumont - 가을향기 <thibautvdumont@gmail.com>
2017-10-21 12:27:37 +0200

    Le plancton
    le plancton (le plancton)

---
+++ Assets/Scripts/RayMouth.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class RayMouth : MonoBehaviour {
+    [System.ComponentModel.DefaultValue(0f)]
+    public float Score
+    {
+        get;
+        private set;
+    }
+
+    List<GameObject> _collidedPlanctons = new List<GameObject>();
+
+    // Use this for initialization
+    void Start ()
+    {
+        Score = 0f;
+        _collidedPlanctons.Clear();
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.tag == "plancton")
+        {
+            if(!_collidedPlanctons.Any(c => object.ReferenceEquals(collision.gameObject, c))) 
+            {
+                _collidedPlanctons.Add(collision.gameObject);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        // To later replace with the sound detection
+        if (true)
+        {
+            foreach (var go in _collidedPlanctons)
+            {
+                Score++;
+                Destroy(go);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.tag == "plancton")
+        {
+            var idToDelete = _collidedPlanctons.Select((c, idx) => new { c, idx }).FirstOrDefault(o => object.ReferenceEquals(o.c, collision.gameObject));
+            if (idToDelete != null) _collidedPlanctons.RemoveAt(idToDelete.idx);
+        }
+    }
+}
