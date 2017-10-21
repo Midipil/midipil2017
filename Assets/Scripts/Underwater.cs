e1912f21751da4f2b6517ace80d7c8cbcfb927b2
Baptiste Moizard <baptiste-moizard@orange.fr>
2017-10-21 20:54:41 +0200

    Settable color for fog

--- Assets/Scripts/Underwater.cs
+++ Assets/Scripts/Underwater.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Underwater : MonoBehaviour {
 
+	public Color fogColor = new Color (0, 0.4f, 0.7f, 1);
+
 	void Start () {
 		//Set the background color
-		GetComponent<Camera> ().backgroundColor = new Color (0, 0.4f, 0.7f, 1);
+		GetComponent<Camera> ().backgroundColor = fogColor;
 
 		RenderSettings.fog = true;
-		RenderSettings.fogColor = new Color (0, 0.4f, 0.7f, 0.6f);
+		RenderSettings.fogColor = fogColor;
 		RenderSettings.fogDensity = 0.04f;
 		RenderSettings.skybox = null;
 	}
