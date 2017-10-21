48b86321a53473ff1576bd7fd4ad9adf01b056e0
Baptiste Moizard <baptiste-moizard@orange.fr>
2017-10-21 16:58:27 +0200

    Shark emit bubbles when dropping

--- Assets/Scripts/Shark.cs
+++ Assets/Scripts/Shark.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class Shark : MonoBehaviour {
 
-    public float speed = 10.0f; // m/s
+	// Speed in m/s
+	public float speed = 10.0f;
+
+	public ParticleSystem bubbles;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 
-    // Update is called once per frame
-    void Update() {
-        if (this.transform.position.y > 0) {
-            this.transform.Translate(new Vector3(0f, -speed*Time.deltaTime, 0f));
-        }
+	// Update is called once per frame
+	void Update () {
+		if (this.transform.position.y > 0) {
+			this.transform.Translate (new Vector3 (0f, -speed * Time.deltaTime, 0f));
+		} else {
+			// Shark is in the ground
+			if (bubbles.isEmitting) {
+				bubbles.Stop ();
+			}
+		}
 	}
 }
